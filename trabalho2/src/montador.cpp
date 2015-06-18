f72e8415cb5a845efadba86d895f7dbfdde74662
sinayra <nayra@hotmail.co.jp>
2015-06-18 15:31:07 -0300

    Esqueci de tirar couts de debug

--- trabalho2/src/montador.cpp
+++ trabalho2/src/montador.cpp
@@ -67,5 +67,5 @@ void monta(vector<string> asmInventado){
         cout << "Erro na geração de " << output << endl;
         remove(output.c_str()); //apague arquivo de saída
     }
-    //remove("pre_processado.txt"); //remoção do arquivo pré-processado temporário
+    remove("pre_processado.txt"); //remoção do arquivo pré-processado temporário
 }
