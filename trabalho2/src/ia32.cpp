91921a3f50da5972b9b960947adfbf0fe574b367
Leandro <leandroferreira@cjr.org.br>
2015-06-21 01:34:19 -0300

    Terminei faltando alguns bugs

--- trabalho2/src/ia32.cpp
+++ trabalho2/src/ia32.cpp
@@ -1,5 +1,16 @@
 
-#include "../include/tradutor.h"
+#include "../include/ia32.h"
 
 
+
+std::string inventadoParaIA32(vector<tipoInstrucao>& instrucoes,string operacao){
+	$achado = false;
+	for(int i = 0; i < (int)instrucoes.size(); i++){
+        if(strcasecmp(instrucoes[i].nome.c_str(), token.c_str()) == 0)
+        	$achado = true;
+    }
+    if($achado){
+
+    }
+}
 
