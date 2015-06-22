0ed95973ba868633972b44d99f7af2c7596dbcb3
sinayra <nayra@hotmail.co.jp>
2015-06-22 15:29:08 -0300

    Quase terminando escreveInteiro

--- trabalho2/srcassembly/lerInteiro.s
+++ trabalho2/srcassembly/lerInteiro.s
@@ -3,31 +3,36 @@
 section .data
 	B	EQU	10
 
-msg_debug1 		db '.DEU ERRADO POXA1: '
+msg_debug1 		db  0dh, 0ah, 'Debug1: '
 	DEBUG1SIZE		EQU $-msg_debug1
-msg_debug2 		db 'DEU MAIS ERRADO AINDA POXA2: '
+msg_debug2 		db 0dh, 0ah, 'Debug2: '
 	DEBUG2SIZE		EQU $-msg_debug2
-msg_debug3 		db 'No final: '
+msg_debug3 		db 0dh, 0ah, 'No final: '
 	DEBUG3SIZE		EQU $-msg_debug3
-msg_debug4 		db 'Loop '
+msg_debug4 		db 0dh, 0ah, 'Loop '
 	DEBUG4SIZE		EQU $-msg_debug4
+msg_debug5 		db 0dh, 0ah, 'Debug3 '
+	DEBUG5SIZE		EQU $-msg_debug5
 
 section .bss
 	RESPOSTA	resb	4		;Usuário determinou que o tamanho da resposta terá no máximo 4 bytes
 	A			resb	4		;coisa do usuário
-
-	numero		resb	1 	; NECESSÁRIO: um byte para mostrar um número em ASCII
-	valor			resb	1 	; NECESSÁRIO: 4 bytes para armazenar inteiro
+	
 	imprime		resb	32	;NECESSÁRIO: jeitinho para imprimir no máximo 32 dígitos, hehe
+	valor			resb	4 	; NECESSÁRIO: 4 bytes para armazenar inteiro
+	numero		resb	1 	; NECESSÁRIO: um byte para mostrar um número em ASCII. Precisa ser a última coisa adicionada no .bss
 
 section .text
 global _start
 	_start:
 	
 		;******************** LER INTEIRO ********************
+		mov eax, [A]
+		mov eax, 0
+		mov [A], eax
 		call lerInteiro
-		;mov eax, [valor]
-		;mov [A], eax		;move para a label que o usuário quer que tenha o inteiro
+		mov eax, [valor]
+		mov [A], eax		;move para a label que o usuário quer que tenha o inteiro
 		;******************** FIM DE LER INTEIRO ********************
 push eax
 push ebx
@@ -42,8 +47,8 @@ mov eax, 4
 
 				mov eax, 4
 				mov ebx, 1
-				mov ecx, valor
-				mov edx, 1
+				mov ecx, A
+				mov edx, 4
 				int 80h
 pop edx
 pop ecx
@@ -60,85 +65,28 @@ pop eax
 ;*****************************************************
 ;			Ler inteiro
 ;	
-;*	 retorna no endereço valor o número que o usuário digitou (em inteiro)
+;*	 retorna no endereço "valor" o número que o usuário digitou (em inteiro)
 ;**********************************************************
 
 	lerInteiro:
-			loopLerInteiro:
-push eax
-push ebx
-push ecx
-push edx
-				mov eax, 4
-				mov ebx, 1
-				mov ecx, msg_debug4
-				mov edx, DEBUG4SIZE
-				int 80h
-
-pop edx
-pop ecx
-pop ebx
-pop eax
-				mov eax, 0
-				mov [numero], eax
-				mov eax, 3
-				mov ebx, 0
-				mov ecx, numero		;Leio um byte em ascii
-				mov edx, 1
-				int 80h
+				
+		mov eax, 3
+		mov ebx, 0
+		mov ecx, numero		;Leio um byte em ascii
+		mov edx, 1
+		int 80h
 			
-				mov ebx, [numero]		;transfere para variável ebx o que está em numero
-
-				cmp ebx, 0x0A		;compara se é igual a enter
-				jz fimLerInteiro
-
-				;sub ebx, 0x30		; tiro a parte ascii
-				add ebx, 0x01
-
-				mov eax, [valor]	;pego o que tiver em [valor]
-				add eax, ebx		;adiciono
-				mov [valor], eax	;retorno para [valor]
-
-push eax
-push ebx
-push ecx
-push edx
-				mov eax, 4
-				mov ebx, 1
-				mov ecx, msg_debug1
-				mov edx, DEBUG1SIZE
-				int 80h
-
-				mov eax, 4
-				mov ebx, 1
-				mov ecx, valor
-				mov edx, 1
-				int 80h
-pop edx
-pop ecx
-pop ebx
-pop eax
-
-				jmp loopLerInteiro	
+		mov ebx, [numero]		;transfere para variável ebx o que está em numero
+		cmp ebx, 0x0A		;compara se é igual a enter
+		jz fimLerInteiro
 			
-			fimLerInteiro:
-push eax
-push ebx
-push ecx
-push edx
-				mov eax, 4
-				mov ebx, 1
-				mov ecx, msg_debug2
-				mov edx, DEBUG2SIZE
-				int 80h
+		;sub ebx, 0x30		; tiro a parte ascii
+		add ebx, 0x01
+		mov eax, [valor]	;pego o que tiver em [valor]
+		add eax, ebx		;adiciono
+		mov [valor], eax	;retorno para [valor]
 
-				mov eax, 4
-				mov ebx, 1
-				mov ecx, valor
-				mov edx, 1
-				int 80h
-pop edx
-pop ecx
-pop ebx
-pop eax
-				ret
+		jmp lerInteiro	
+		
+		fimLerInteiro:
+			ret
--- trabalho2/srcassembly/teste1.s
+++ trabalho2/srcassembly/teste1.s
@@ -3,73 +3,103 @@
 section .data
 	B	EQU	10
 
-msg_debug1 		db '.DEU ERRADO POXA1: '
+msg_debug1 		db  0dh, 0ah, 'Debug1: '
 	DEBUG1SIZE		EQU $-msg_debug1
-msg_debug2 		db 'DEU MAIS ERRADO AINDA POXA2: '
+msg_debug2 		db 0dh, 0ah, 'Debug2: '
 	DEBUG2SIZE		EQU $-msg_debug2
-msg_debug3 		db 'No final: '
+msg_debug3 		db 0dh, 0ah, 'No final: '
 	DEBUG3SIZE		EQU $-msg_debug3
+msg_debug4 		db 0dh, 0ah, 'Loop '
+	DEBUG4SIZE		EQU $-msg_debug4
+msg_debug5 		db 0dh, 0ah, 'Debug3 '
+	DEBUG5SIZE		EQU $-msg_debug5
 
 section .bss
 	RESPOSTA	resb	4		;Usuário determinou que o tamanho da resposta terá no máximo 4 bytes
 	A			resb	4		;coisa do usuário
-
-	numero		resb	1 	; NECESSÁRIO: um byte para mostrar um número em ASCII
-	valor			resb	1 	; NECESSÁRIO: 4 bytes para armazenar inteiro
-	imprime		resb	32	;NECESSÁRIO: jeitinho para imprimir no máximo 32 dígitos, hehe
+	
+	imprime		resb	32	; NECESSÁRIO: jeitinho para imprimir no máximo 32 dígitos, hehe
+	valor			resb	4 	; NECESSÁRIO: 4 bytes para armazenar inteiro
+	digitos		resb	4	; NECESSÁRIO: 4 bytes para contar quantidade de dígitos
+	numero		resb	1 	; NECESSÁRIO: um byte para mostrar um número em ASCII. Precisa ser a última coisa adicionada no .bss
 
 section .text
 global _start
+	_start:
 	
-	;coloca na pilha o número inteiro que o usuário digitou
-	;retorna no endereço valor o número que o usuário digitou (em inteiro)
+		;******************** LER INTEIRO ********************
+		mov eax, [valor]
+		mov eax, 0
+		mov [valor], eax	;valor neutro
+		mov eax, 1
+		mov [digitos], eax	;valor neutro
+		call lerInteiro
+		mov eax, [valor]
+		mov [A], eax		;move para a label que o usuário quer que tenha o inteiro
+		;******************** FIM DE LER INTEIRO ********************
+
+		;******************** ESCREVER INTEIRO ********************
+		mov eax, [A]		;label que contenha o número que o usuário quer imprimir
+		mov ebx, imprime
+		call escreverInteiro
+		mov eax, 4
+		mov ebx, 1
+		mov ecx, imprime			;label que tem a resposta em ascii
+		mov edx, 32
+		int 80h	
+		;******************** FIM DE ESCREVER INTEIRO ********************
+
+		; Encerra programa
+		mov eax, 1
+		mov ebx, 0
+		int 80h
+
+
+;*****************************************************
+;			Ler inteiro
+;	
+;*	 retorna no endereço "valor" o número que o usuário digitou (em inteiro)
+;**********************************************************
+
 	lerInteiro:
-			loopLerInteiro:
-				mov eax, 0
-				mov [numero], eax
-				mov eax, 3
-				mov ebx, 0
-				mov ecx, numero		;Leio um byte em ascii
-				mov edx, 1
-				int 80h
+				
+		mov eax, 3
+		mov ebx, 0
+		mov ecx, numero		;Leio um byte em ascii
+		mov edx, 1
+		int 80h
 			
-				mov ebx, [numero]		;transfere para variável ebx o que está em numero
-
-				cmp ebx, 0x0A		;compara se é igual a enter
-				jz fimLerInteiro
-
-				;sub ebx, 0x30		; tiro a parte ascii
-				add ebx, 0x01
-
-				mov eax, [valor]	;pego o que tiver em [valor]
-				add eax, ebx		;adiciono
-				mov [valor], eax	;retorno para [valor]
-
-push eax
-push ebx
-push ecx
-push edx
-				mov eax, 4
-				mov ebx, 1
-				mov ecx, msg_debug1
-				mov edx, DEBUG1SIZE
-				int 80h
-
-				mov eax, 4
-				mov ebx, 1
-				mov ecx, valor
-				mov edx, 1
-				int 80h
-pop edx
-pop ecx
-pop ebx
-pop eax
-
-				jmp loopLerInteiro	
+		mov ebx, [numero]		;transfere para variável ebx o que está em numero
+		cmp ebx, 0x0A		;compara se é igual a enter
+		jz fimLerInteiro
 			
-			fimLerInteiro:
-				ret
+		sub ebx, 0x30		; tiro a parte ascii
+		mov eax, [valor]	;pego o que tiver em [valor]
+		mov ecx, [digitos]
+		mul ecx
+		add eax, ebx		;adiciono
+		mov [valor], eax	;retorno para [valor]
 
+		mov eax, [digitos]
+		mov ecx, 10
+		mul ecx
+		mov [digitos], eax
+		
+		mov [digitos], ecx
+
+		jmp lerInteiro	
+		
+		fimLerInteiro:
+			ret
+
+;*****************************************************
+;			Escreve Inteiro
+;	Arg: 
+;	eax: número
+;	ebx: endereço de memória
+;	
+;	 escreverInteiro vai colocar o número em ascii na forma little endian, colocando este número no endereço ebx
+;**********************************************************
 	; eax: número
 	;ebx: endereço de memória
 	;escreverInteiro vai colocar o número em ascii na forma little endian, colocando este número no endereço ebx
@@ -100,48 +130,3 @@ pop eax
 				mov [ebx], ecx
 
 				ret
-
-
-	_start:
-	
-		;******************** LER INTEIRO ********************
-		call lerInteiro
-		;mov eax, [valor]
-		;mov [A], eax		;move para a label que o usuário quer que tenha o inteiro
-		;******************** FIM DE LER INTEIRO ********************
-push eax
-push ebx
-push ecx
-push edx
-
-mov eax, 4
-				mov ebx, 1
-				mov ecx, msg_debug3
-				mov edx, DEBUG3SIZE
-				int 80h
-
-				mov eax, 4
-				mov ebx, 1
-				mov ecx, valor
-				mov edx, 1
-				int 80h
-pop edx
-pop ecx
-pop ebx
-pop eax
-		
-		;******************** ESCREVER INTEIRO ********************
-;		mov eax, [valor]		;label que contenha o número que o usuário quer imprimir
-;		mov ebx, imprime
-;		call escreverInteiro
-;		mov eax, 4
-;		mov ebx, 1
-;		mov ecx, imprime			;label que tem a resposta em ascii
-;		mov edx, 32
-;		int 80h	
-		;******************** FIM DE ESCREVER INTEIRO ********************
-
-		; Encerra programa
-		mov eax, 1
-		mov ebx, 0
-		int 80h
