f544db747fbbd078356d2cce357cf46ac7a2a21e
sinayra <nayra@hotmail.co.jp>
2015-06-26 11:29:57 -0300

    Adicionado instrução STOP

--- trabalho2/srcassembly/módulos/lerInteiro.s
+++ trabalho2/srcassembly/módulos/lerInteiro.s
@@ -19,27 +19,18 @@ msg_debug7		db 0dh, 0ah, 'Errado!'
 	DEBUG7SIZE		EQU $-msg_debug7
 
 section .bss
-	RESPOSTA	resb	4		;Usuário determinou que o tamanho da resposta terá no máximo 4 bytes
-	A			resb	4		;coisa do usuário
-	
 	imprime		resb	9	; NECESSÁRIO: jeitinho para imprimir no máximo 9 dígitos, hehe
 	valor			resb	4 	; NECESSÁRIO: 4 bytes para armazenar inteiro
 	casas			resb	4	; NECESSÁRIO: casas decimais
 	digitos		resb	4	; NECESSÁRIO: 4 bytes para contar quantidade de dígitos
 	numero		resb	1 	; NECESSÁRIO: um byte para mostrar um número em ASCII. Precisa ser a última coisa adicionada no .bss
 
+	RESPOSTA	resb	4		;Usuário determinou que o tamanho da resposta terá no máximo 4 bytes
+	A			resb	4		;coisa do usuário
+
 section .text
 global _start
 	_start:
-		;******************** ZERANDO VARIÁVEIS ********************
-		mov eax, 0
-		mov [valor], eax	;valor neutro
-		mov [imprime], eax	;valor neutro
-		mov [numero], eax	;valor neutro
-		mov [digitos], eax	;valor neutro
-		mov eax, 1
-		mov [casas], eax	;valor neutro
-		;******************** FIM DE ZERANDO VARIÁVEIS ********************		
 
 		;******************** LER INTEIRO ********************
 		call lerInteiro
@@ -47,6 +38,45 @@ global _start
 		mov [A], eax		;move para a label que o usuário quer que tenha o inteiro
 		;******************** FIM DE LER INTEIRO ********************
 
+		mov eax, [A]
+		cmp eax, 123456
+		je certo
+		
+		errado:
+			push eax
+			push ebx
+			push ecx
+			push edx
+				
+				mov eax, 4
+				mov ebx, 1
+				mov ecx, msg_debug7
+				mov edx, DEBUG7SIZE
+				int 80h	
+			pop edx
+			pop ecx
+			pop ebx
+			pop eax
+		; Encerra programa
+		mov eax, 1
+		mov ebx, 0
+		int 80h
+
+		certo:
+			push eax
+			push ebx
+			push ecx
+			push edx
+				
+				mov eax, 4
+				mov ebx, 1
+				mov ecx, msg_debug6
+				mov edx, DEBUG6SIZE
+				int 80h	
+			pop edx
+			pop ecx
+			pop ebx
+			pop eax
 		; Encerra programa
 		mov eax, 1
 		mov ebx, 0
@@ -61,37 +91,40 @@ global _start
 ;**********************************************************
 
 	lerInteiro:
+		;******************** ZERANDO VARIÁVEIS ********************
 		mov eax, 0
-		mov [numero], eax
+		mov [valor], eax	;valor neutro
+		mov [numero], eax	;valor neutro
+		mov eax, 1
+		mov [casas], eax	;valor neutro
+		;******************** FIM DE ZERANDO VARIÁVEIS ********************
 
-		mov eax, 3
-		mov ebx, 0
-		mov ecx, numero		;Leio um byte em ascii
-		mov edx, 1
-		int 80h
+		lerInteiroLoop:
+			mov eax, 0
+			mov [numero], eax
+
+			mov eax, 3
+			mov ebx, 0
+			mov ecx, numero		;Leio um byte em ascii
+			mov edx, 1
+			int 80h
 			
-		mov ebx, [numero]		;transfere para variável ebx o que está em numero
-		cmp ebx, 0x0A		;compara se é igual a enter
-		jz fimLerInteiro
+			mov ebx, [numero]		;transfere para variável ebx o que está em numero
+			cmp ebx, 0x0A		;compara se é igual a enter
+			jz fimLerInteiro
 			
-		sub ebx, 0x30		; tiro a parte ascii
-		mov eax, [valor]	;pego o que tiver em [valor]
-		mov ecx, [casas]
-		mul ecx
-		add eax, ebx		;adiciono
-		mov [valor], eax	;retorno para [valor]
-		mov eax, [digitos]
-		inc eax
-		mov [digitos], eax
+			sub ebx, 0x30		; tiro a parte ascii
+			mov eax, [valor]	;pego o que tiver em [valor]
+			mov ecx, [casas]
+			mul ecx
+			add eax, ebx		;adiciono
+			mov [valor], eax	;retorno para [valor]
 
-		mov eax, 10
-		mov [casas], eax
+			mov eax, 10
+			mov [casas], eax
 
-		jmp lerInteiro	
+			jmp lerInteiroLoop	
 		
 		fimLerInteiro:
-			mov eax, [digitos]
-			dec eax
-			mov [digitos], eax
 
 			ret
